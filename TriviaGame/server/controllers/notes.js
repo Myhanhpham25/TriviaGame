5ebe091b90ee44fb2d1ed5f2661a48d097eea100
Myhanhpham25 <myhanhpham25@gmail.com>
2017-10-20 14:32:10 -0700

    triviagame inital

---
+++ TriviaGame/server/controllers/notes.js
@@ -0,0 +1,10 @@
+prompt was the way to show the message. 
+
+embedded document for the questions so it can be put into an array and then shuffle.
+
+random the array- slice or shuffle 
+
+shuffle the array and take the many item off the front or the back 
+have to find the shuffling algorithm 
+
+
