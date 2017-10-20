aceffae7335fd84770721a085aad82d60951032a
Myhanhpham25 <myhanhpham25@gmail.com>
2017-10-20 14:41:35 -0700

    trivia

--- beltProject/client/src/app/users/question.ts
+++
@@ -1,7 +0,0 @@
-export class Question {
-	question: string
-	answer : string
-	fake1: string
-	fake2: string
-	createAt: Date 
-}
--- beltProject/client/src/app/users/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-	_id : number 
-	name: string
-	email: string
-	password: string
-	score: number 
-	createdAt: Date
-	updatedAt: Date 
-}
